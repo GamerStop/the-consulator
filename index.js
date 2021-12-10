095d7eaae25c96908e74857826c4b3b7a861034b
GamerStop <yt.gamerstop.yt@gmail.com>
2021-12-10 16:59:40 +0200

    same running process but less lines

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ app.use(express.json());
 const Credentials = require('./models/credentials')
 
 // MongoDB Connection
-mongoose
-    .connect(require('./config/keys').mongoURI,{ useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log('MongoDB Connected, localhost:8080. - ' + new Date()))
-    .catch(err => console.log(err));
+mongoose.connect(require('./config/keys').mongoURI,{ useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false}).catch(err => console.log(err));
 
 // Authentication Test
 app.post('/api/', (req, res) => { Credentials.countDocuments({ key : req.query.key }, (err, count) => {if (count === 1) {res.json({message:'authenticated'})} else {res.json({message:'bad credentials'})}})}) 
@@ -23,4 +20,4 @@ app.post('/api/', (req, res) => { Credentials.countDocuments({ key : req.query.k
 app.post('/api/animals/cat-facts/', (req, res) => {Credentials.countDocuments({ key : req.query.key }, (err, count) => {if (count === 1) {res.json({data : require('./api-data/animals/cat-facts.js').data[Math.floor(Math.random() * require('./api-data/animals/cat-facts.js').data.length)]})} else {res.json({message:'bad credentials'})}})})
 
 // Listen
-app.listen(8080)
+app.listen(8080, console.log('MongoDB Connected, localhost:8080. - ' + new Date()))
